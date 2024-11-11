9a54a304496780d10bd74ac7abe79b9db9007c85
MilesOCLol <milesoconnorme@gmail.com>
2024-11-11 13:37:45 -0800

    Added randomness and order

--- Reversed/Assets/CardBackManager.cs
+++ Reversed/Assets/CardBackManager.cs
@@ -8,9 +8,19 @@ public class CardBackManager : MonoBehaviour
     public GameObject CardPrefab;
     public GameObject PrefabParent;
 
+    public CardData CardObj;
+    public int increment = 0;
+
     private void OnMouseDown() {
-        Debug.Log("clicked");
+        increment++;
+
         GameObject NewCard = Instantiate(CardPrefab, Vector3.zero, Quaternion.identity);
         NewCard.transform.SetParent(PrefabParent.transform);
+        
+        SpriteRenderer CardRenderer = NewCard.transform.GetChild(0).GetComponent<SpriteRenderer>();
+
+        CardRenderer.sortingOrder = increment;
+        CardObj.number = Random.Range(1, 13);
+        CardObj.suit = Random.Range(1, 4);
     }
 }
