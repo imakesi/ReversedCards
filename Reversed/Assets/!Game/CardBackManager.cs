f4283f3a0d68cb53c974a515895f2cc9f6c678ce
miral.tri <elik092005@gmail.com>
2024-11-20 17:30:15 -0800

    struct stuff

--- Reversed/Assets/!Game/CardBackManager.cs
+++ Reversed/Assets/!Game/CardBackManager.cs
@@ -11,13 +11,28 @@ public class CardBackManager : MonoBehaviour
     public CardData CardObj;
     public int increment = 0;
 
-    private void OnMouseDown() {
+    //public struct Card() {
+        //Card(int suit, int number);
+    //}
+
+    private List<GameObject> DeckLeft = new List<GameObject>();
+
+    private void Start() {
+        
+    }
+
+    private void ResetDeck() {
+       
+    }
+
+    public void MakeCard() { // runs with button
         increment++;
 
         GameObject NewCard = Instantiate(CardPrefab, Vector3.zero, Quaternion.identity);
         NewCard.transform.SetParent(PrefabParent.transform);
-        
-        SpriteRenderer CardRenderer = NewCard.transform.GetChild(0).GetComponent<SpriteRenderer>();
+
+        // SpriteRenderer CardRenderer = NewCard.transform.GetChild(0).GetComponent<SpriteRenderer>();
+        SpriteRenderer CardRenderer = NewCard.GetComponentInChildren<SpriteRenderer>();
 
         CardRenderer.sortingOrder = increment;
         CardObj.number = Random.Range(1, 13);
