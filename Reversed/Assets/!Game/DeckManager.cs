47a227db164c29a3d5a6f2f3490ee5b29b6abc2f
miral.tri <elik092005@gmail.com>
2024-11-27 17:30:30 -0800

    did stuff that is stuff

---
+++ Reversed/Assets/!Game/DeckManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DeckManager : MonoBehaviour
+{
+    public GameObject CardPrefab;
+    public GameObject PrefabParent;
+
+    public CardData CardObj;
+
+    private SpriteRenderer LastCardRenderer;
+
+    public struct CardStruct {
+        public CardStruct(int suit, int number) {}
+    }
+
+    private List<int[]> DeckLeft = new List<int[]>(); // int[suit,number] = card data but array
+
+    private void Start() {
+        ResetDeck();
+    }
+
+    private void ResetDeck() {
+        DeckLeft = new List<int[]>();
+        for (int suit = 1; suit < 5; suit++) {
+            for (int number = 1; number < 14; number++) {
+                int[] NewCard = {suit, number};
+                DeckLeft.Add(NewCard);
+            }
+        }
+    }
+
+    public void MakeCard() { // runs with button
+        GameObject NewCard = Instantiate(CardPrefab, Vector3.zero, Quaternion.identity);
+        NewCard.transform.SetParent(PrefabParent.transform);
+
+        // SpriteRenderer CardRenderer = NewCard.transform.GetChild(0).GetComponent<SpriteRenderer>();
+        SpriteRenderer CardRenderer = NewCard.GetComponentInChildren<SpriteRenderer>();
+        if (LastCardRenderer) {
+            LastCardRenderer.sortingOrder = 0;
+        }
+
+        CardRenderer.sortingOrder = 1;
+
+        if(DeckLeft.Count == 0) {
+            Debug.Log("ran out of cards");
+            ResetDeck();
+        }
+
+        int[] CardPair = DeckLeft[Random.Range(0, DeckLeft.Count)];
+        DeckLeft.Remove(CardPair);
+
+        CardObj.suit = CardPair[0];
+        CardObj.number = CardPair[1];
+
+        LastCardRenderer = NewCard.GetComponentInChildren<SpriteRenderer>();
+    }
+}
