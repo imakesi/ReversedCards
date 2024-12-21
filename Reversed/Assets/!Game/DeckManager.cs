d11f469ceede2a1ef94025917948336c7051fafe
miral.tri <elik092005@gmail.com>
2024-12-20 18:30:16 -0700

    this is a commit

--- Reversed/Assets/!Game/DeckManager.cs
+++ Reversed/Assets/!Game/DeckManager.cs
@@ -26,7 +26,7 @@ public class DeckManager : MonoBehaviour
     private void Start() {
         ResetDeck();
 
-        for(int i = 0; i < 8; i++) {
+        for(int i = 0; i < 10; i++) {
             GameObject NewCard1 = MakeCard();
             Player1.AddCard(NewCard1);
             GameObject NewCard2 = MakeCard();
--- Reversed/Assets/!Game/Player.cs
+++ Reversed/Assets/!Game/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -17,27 +18,42 @@ public class Player : MonoBehaviour
 
     private void DisplayHand() {
         // min = current page number minus 1 times 5 (last page which we don't want to display unless we're on the first page) and then plus one
+        // kinda like fibbonacci thing where you go down until 1 ^
         // max = min + 4
         // get range somehow [min, min+1, min+2, min+3, max]
         // use indexes and get the transform of the cards in the hand list and move them to 10,10,0
 
-        // card in
 
-        int Adder = (CurrentPage - 1) * 5;
+        int min;
 
-        for (int j = 0; j < 5; j++) {
-            for (int i = 0; i < 5; i++) {
-                if (i + Adder >= Hand.Count) { break; }
-                Hand[i + Adder].transform.position = CardPositions[i].position;
-            } //for (int i = 0; i < Hand.Count % 5; i++) {
-            //    Hand[i + Adder].transform.position = CardPositions[i + Adder].position;
-            //}
+        if (CurrentPage > 1) {
+            min = ((CurrentPage - 1) * 5) + 1;
+        } else { min = 0; }
+
+        List<int> RangeMM = new List<int>();
+
+        // add cards to RangeMM that should be displayed on page
+        for (int i = min; i < min + 5; i++) {
+            if (Hand.Count < i) { break; }
+
+            RangeMM.Add(i);
         }
 
+        for (int i = 0; i < RangeMM.Count; i++) {
+            print(string.Join(",", RangeMM));
+            int MyOtherThing = RangeMM[i];
+            GameObject errorpossible = Hand[MyOtherThing];
+            errorpossible.transform.position = CardPositions[i].position;
+        }
 
-        // card out
+        //int Adder = (CurrentPage - 1) * 5;
 
-        int min = ((CurrentPage - 1) * 5) + 1; // make this only if current page nums greater than 1
+        //for(int j = 0; j < 5; j++) {
+        //    for(int i = 0; i < 5; i++) {
+        //        if (i + Adder >= Hand.Count) { break; }
+        //        Hand[i + Adder].transform.position = CardPositions[i].position;
+        //    }
+        //}
     }
 
     private void Update() {
