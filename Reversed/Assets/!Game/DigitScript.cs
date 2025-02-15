17b6f7e8af23e061245e4c6c1c3297b01f6b95f3
miral.tri <elik092005@gmail.com>
2025-02-14 19:31:35 -0800

    sidebar

---
+++ Reversed/Assets/!Game/DigitScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DigitScript : MonoBehaviour
+{
+    public int value = 0;
+
+    private void Start() {
+        // get sprite renderer, render value from font folder
+    }
+}
